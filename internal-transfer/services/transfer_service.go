1a6884e3ac14ac63b439093a7cfacf50357ee3c4
Paparao Ravulapalli <r.paparaoktsr@gmail.com>
2025-07-30 08:58:24 +0530

    Refactor TransferFunds function to include transaction logging

--- internal-transfer/services/transfer_service.go
+++ internal-transfer/services/transfer_service.go
@@ -8,6 +8,52 @@ import (
 )
 
 
+// func TransferFunds(fromID, toID int64, amount float64) error {
+// 	tx, err := db.DB.Begin()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	defer func() {
+// 		if p := recover(); p != nil {
+// 			tx.Rollback()
+// 			panic(p)
+// 		}
+// 	}()
+
+// 	var fromBalance float64
+// 	err = tx.QueryRow("SELECT balance FROM accounts WHERE account_id = $1 FOR UPDATE", fromID).Scan(&fromBalance)
+// 	if err != nil {
+// 		tx.Rollback()
+// 		return err
+// 	}
+
+// 	if fromBalance < amount {
+// 		tx.Rollback()
+// 		return errors.New("insufficient funds")
+// 	}
+
+// 	_, err = tx.Exec("UPDATE accounts SET balance = balance - $1 WHERE account_id = $2", amount, fromID)
+// 	if err != nil {
+// 		tx.Rollback()
+// 		return err
+// 	}
+
+// 	_, err = tx.Exec("UPDATE accounts SET balance = balance + $1 WHERE account_id = $2", amount, toID)
+// 	if err != nil {
+// 		tx.Rollback()
+// 		return err
+// 	}
+
+// 	err = tx.Commit()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	log.Printf("Transferred %.2f from account %d to %d", amount, fromID, toID)
+// 	return nil
+// }
+
 func TransferFunds(fromID, toID int64, amount float64) error {
 	tx, err := db.DB.Begin()
 	if err != nil {
@@ -45,6 +91,14 @@ func TransferFunds(fromID, toID int64, amount float64) error {
 		return err
 	}
 
+	_, err = tx.Exec(`
+		INSERT INTO transactions (source_account_id, destination_account_id, amount)
+		VALUES ($1, $2, $3)`, fromID, toID, amount)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
 	err = tx.Commit()
 	if err != nil {
 		return err
@@ -53,3 +107,4 @@ func TransferFunds(fromID, toID int64, amount float64) error {
 	log.Printf("Transferred %.2f from account %d to %d", amount, fromID, toID)
 	return nil
 }
+
